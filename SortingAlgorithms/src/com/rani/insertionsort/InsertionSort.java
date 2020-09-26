ed01b2f43ea9eb4274438c65ed41f2e6cd19a90b
Gautami Khante <33024174+GautamiKhante@users.noreply.github.com>
2020-09-27 01:24:11 +0530

    Insertion Sort Java program.

---
+++ SortingAlgorithms/src/com/rani/insertionsort/InsertionSort.java
@@ -0,0 +1,22 @@
+package com.rani.insertionsort;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+        int[] intArray = {34, 56, 23, 1, -65, -2};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+            int newElement = intArray[firstUnsortedIndex]; // saving the unsorted element before replacing
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && intArray[i] > intArray[i - 1]; i--) {
+                intArray[i] = intArray[i - 1]; // replacing smaller element to the left
+            }
+            intArray[i] = newElement; // putting back the element in the position of the first unsorted index
+
+        }
+        for (int i = 0 ; i< intArray.length ; i++){
+            System.out.println("Sorted Array " + intArray[i]);
+        }
+    }
+}
